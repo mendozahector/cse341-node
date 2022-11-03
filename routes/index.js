2119a92734ef3fd4c634550e9d053636bb15b924
Hector Mendoza <hectormendoza@byui.edu>
2022-11-03 00:17:11 -0400

    nodemon

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Lex Fridman')
+    res.send('Lex Fridman!!!')
   })
 
 module.exports = router;
