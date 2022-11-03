83dffe1b9f95737f798ee1139d0b44eaf6b16a02
Hector Mendoza <hectormendoza@byui.edu>
2022-11-03 01:33:45 -0400

    fixed main controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
-
-const getAll = async (req, res) => {
+const getAll = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(["Lext Fridman"]);
+    res.status(200).json({"message": "Hello there!", "to": "Lex Fridman"});
 };
+
+module.exports = {
+    getAll
+  };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const mainController = require('../controllers/index');
+const mainController = require('../controllers');
 router.get('/', mainController.getAll);
+
+// router.get('/', (req, res) => {
+//   res.send('Lex Fridman!!!')
+// })
 
 module.exports = router;
