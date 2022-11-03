0326ab92d3b83384be4bc3605b134215e07d4929
Hector Mendoza <hectormendoza@byui.edu>
2022-11-03 00:55:15 -0400

    updated env port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
